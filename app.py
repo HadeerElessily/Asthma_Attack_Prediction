0da8a7d9905eade884f8163678f96b01be1ccdb4
HadeerElessily <121236831+HadeerElessily@users.noreply.github.com>
2023-05-24 02:03:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,36 @@
 from flask import Flask, jsonify, request, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import pandas as pd
 import joblib
 
 app = Flask(__name__)
 CORS(app)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///asthma_data.db'  # Update the database URI if needed
+db = SQLAlchemy(app)
 
 model = joblib.load('RFclf.joblib')
+
+
+class AsthmaData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cough = db.Column(db.String(50))
+    phlegm = db.Column(db.String(50))
+    wheeze = db.Column(db.String(50))
+    Dysp = db.Column(db.String(50))
+    Exe = db.Column(db.String(50))
+    AR = db.Column(db.String(50))
+    par2 = db.Column(db.String(50))
+    RIr = db.Column(db.String(50))
+    REm = db.Column(db.String(50))
+    Ecz = db.Column(db.String(50))
+    Food = db.Column(db.String(50))
+    Cold = db.Column(db.String(50))
+    AP = db.Column(db.String(50))
+    Psmoke = db.Column(db.String(50))
+    Animal = db.Column(db.String(50))
+    Damp = db.Column(db.String(50))
+    asthma = db.Column(db.String(50))
 
 
 @app.route('/', methods=['POST'])
@@ -33,6 +56,33 @@ def predict():
 
         print('Prediction:', prediction[0])  # Print the prediction
 
+        # Create a new AsthmaData object
+        asthma_data = AsthmaData(
+            cough=data['cough'],
+            phlegm=data['phlegm'],
+            wheeze=data['wheeze'],
+            Dysp=data['Dysp'],
+            Exe=data['Exe'],
+            AR=data['AR'],
+            par2=data['par2'],
+            RIr=data['RIr'],
+            REm=data['REm'],
+            Ecz=data['Ecz'],
+            Food=data['Food'],
+            Cold=data['Cold'],
+            AP=data['AP'],
+            Psmoke=data['Psmoke'],
+            Animal=data['Animal'],
+            Damp=data['Damp'],
+            asthma=result  
+        )
+
+        with app.app_context():
+            # Add the AsthmaData object to the database session
+            db.session.add(asthma_data)
+
+            # Commit the changes to the database
+            db.session.commit()
 
         return jsonify({'result': result}), 200, {'Content-Type': 'application/json'}
 
@@ -42,6 +92,16 @@ def predict():
 
         # Return the error message as a JSON response
         return jsonify({'error': str(e)}), 400, {'Content-Type': 'application/json'}
+
+
+@app.route('/dashboard')
+def dashboard():
+    with app.app_context():
+        # Retrieve all data from the database
+        asthma_data = AsthmaData.query.all()
+    
+    # Render the dashboard template with the data
+    return render_template('dashboard.html', asthma_data=asthma_data)
 
 
 if __name__ == '__main__':
